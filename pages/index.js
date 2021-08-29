e4c5786a2a88270dbdcaff1bce70d537f8f5c838
joaoalbuquerque <joao.albuquerque@teclat.com.br>
2021-08-29 15:58:07 -0300

    Show date from json

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,24 @@
+const api = "https://pomber.github.io/covid19/"
+const DATA = api + "timeseries.json"
+
+function useData() {
+  const [data, setData] = React.useState()
+  React.useEffect(() => {
+    fetch(DATA)
+      .then(response => response.json())
+      .then(data => setData(data))
+  }, [])
+  return data
+}
+
 export default function HomePage() {
-    return <h1>Hello</h1>
+  const data = useData()
+  if (!data) {
+    return <h1>Loading...</h1>
   }
-  
+  const countries = Object.keys(data)
+  const aCountry = data[countries[0]]
+  const { date } = aCountry[aCountry.length - 1]
+  return <h2>Coronavirus {date}</h2>
+}
+
